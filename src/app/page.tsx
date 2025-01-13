b47751b891475fd48dbe29879fd1e5732fc33f6e
unknown <antu.haks@gmail.com>
2025-01-13 15:21:16 +0600

    Implemented Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,40 @@
 import Image from "next/image";
+import BackgroundImage from "@/assets/shaah-shahidh--subrrYxv8A-unsplash.jpg";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <section className="relative bg-gradient-to-r from-gray-800 via-gray-900 to-black   px-10 lg:px-16 xl:px-24 mx-auto h-[80vh] text-white overflow-hidden">
+      {/* Background Image */}
+      <div className="absolute inset-0">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          className="object-cover object-center w-full h-full"
+          src={BackgroundImage}
+          alt="Heavy Marine Logistics Background"
+          layout="fill"
           priority
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <div className="absolute inset-0 bg-black opacity-30"></div>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      {/* Content */}
+      <div className="relative z-10 flex flex-col lg:flex-row items-center justify-center gap-6 h-full text-center md:text-left lg:mt-[10vh]">
+        <h1 className="text-3xl md:text-4xl xl:text-5xl font-bold leading-tight uppercase tracking-wide" style={{ wordSpacing: "0.3em" }}>
+          HEAVY MARINE LOGISTICS
+        </h1>
+
+        <div className="mt-[19.5vh] xl:mt-[13.5vh] w-1/3">
+          <p className="text-base md:text-md text-gray-300 mx-auto w-full md:max-w-xl lg:max-w-lg xl:max-w-md text-justify mb-4">
+            Megaline, headquartered in Seoul, South Korea, is one of the leading service providers of heavy marine transportation. We own and operate four heavy transport vessels, comprising three world-class self-propelled deck carriers and one semi-submersible.
+          </p>
+
+          <div className="flex justify-center lg:justify-end">
+            <Link href="#" className="bg-[#094d82] text-white py-1 px-6 text-base transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg">
+              More Info
+            </Link>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </section>
   );
 }
