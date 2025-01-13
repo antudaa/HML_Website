35e87bf87d05506fea86abe4741686166a75e98a
unknown <antu.haks@gmail.com>
2025-01-13 15:21:51 +0600

    Implemented Homepage

---
+++ src/utils/menuItems.ts
@@ -0,0 +1,26 @@
+export const navLinks = [
+    {
+        name: "COMPANY",
+        link: "#",
+    },
+    {
+        name: "HSEQ",
+        link: "#",
+    },
+    {
+        name: "FLEET",
+        link: "#",
+    },
+    {
+        name: "PROJECT",
+        link: "#",
+    },
+    {
+        name: "ENGINEERING",
+        link: "#",
+    },
+    {
+        name: "CONTACT US",
+        link: "#",
+    }
+];
